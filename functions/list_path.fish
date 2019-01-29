0304c4d0653a8b6e172ade6d3a9fa667643d46d6
Quyen Truong <quyent22@cs.washington.edu>
2019-01-29 11:13:35 -0800

    Update list_path.fish

--- functions/list_path.fish
+++ functions/list_path.fish
@@ -1,7 +1,15 @@
-function list_path
+function list_path  --description "list_path"
   echo "ID"\t"Path"
   set i 1
+  # set color1 (printf "%02x%02x%02x" (random 100 150) (random 100 150) (random 100 150))
+  # set color2 (printf "%02x%02x%02x" (random 150 255) (random 150 255) (random 150 255))
+  set color1 (random choice {brred, brgreen, bryellow, brblue, brmagenta, brcyan, brwhite})
+  set color2 (random choice {brred, brgreen, bryellow, brblue, brmagenta, brcyan, brwhite})
   for p in $PATH
+    set_color "$color1"
+    if test (math "$i%2") -eq 0
+        set_color "$color2"
+    end
   	echo $i\t$p
   	set i (math "$i+1")
   end
