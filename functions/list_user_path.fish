c79b9516cad93f2b4c0ca421d18e27b0d8d6797a
Quyen Truong <quyent22@cs.washington.edu>
2019-01-29 12:44:00 -0800

    Update list_user_path.fish

--- functions/list_user_path.fish
+++ functions/list_user_path.fish
@@ -1,7 +1,13 @@
-function list_user_path
+function list_user_path -d "list_user_path"
   echo "ID"\t"Path"
   set i 1
+  set color1 (random choice {brred, brgreen, bryellow, brblue, brmagenta, brcyan, brwhite})
+  set color2 (random choice {brred, brgreen, bryellow, brblue, brmagenta, brcyan, brwhite})
   for p in $fish_user_paths
+    set_color "$color1"
+    if test (math "$i%2") -eq 0
+        set_color "$color2"
+    end
   	echo $i\t$p
   	set i (math "$i+1")
   end
