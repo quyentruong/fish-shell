765d038ff445d464729136bdb374c72089f715f7
Quyen Truong <quyent22@cs.washington.edu>
2019-01-30 09:25:23 -0800

    Update delete_path.fish

--- functions/delete_path.fish
+++ functions/delete_path.fish
@@ -3,5 +3,27 @@ function delete_path
     echo "Usage: Run list_path to choose ID of path to delete"
     return 1
   end
-  set -e PATH[$argv]
+
+  if echo "$argv" | grep -q -E '^[0-9]+$'
+    if test -z "$prompt"
+        set prompt "Continue?"
+    end
+
+    while true
+        read -p 'set_color green; echo -n "$prompt [y/N]: "; set_color normal' -l confirm
+
+        switch $confirm
+            case Y y
+                set -e PATH[$argv]
+                return 0
+            case '' N n
+                return 1
+        end
+    end
+
+  else
+    echo "Only accpet number"
+    return 1
+  end
+
 end
